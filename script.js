d4ffb775c09bbd9062ae82c3dff5655a71db6826
Somayeh Khanbabaei <somayeh.khanbabaei21@gmail.com>
2024-04-24 15:57:59 +0200

    add menu fade animation

--- script.js
+++ script.js
@@ -62,3 +62,54 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
     })
   }
 })
+
+
+// Tabbed component 
+
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', function(e) {
+  const clicked = e.target.closest('.operations__tab') /* closest parent base to what i write in ()*/
+  if(!clicked) return;
+
+  tabsContent.forEach(c=> c.classList.remove('operations__content--active'))
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  clicked.classList.add('operations__tab--active')
+
+  document.querySelector
+    (`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active')
+})
+
+
+
+// Menu fade animation
+const nav = document.querySelector('.nav');
+
+const handleHover = function(e, opacity) {
+  if(e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = opacity;
+    })
+    logo.style.opacity = opacity;
+  }
+}
+
+nav.addEventListener('mouseover', function(e) {
+  handleHover(e, .5)
+})
+
+nav.addEventListener('mouseout', function(e) {
+  handleHover(e, 1)
+})
+
+// nav.addEventListener('mouseover', handleHover.bind( 0.5))
+// nav.addEventListener('mouseout', handleHover.bind(1))
+
